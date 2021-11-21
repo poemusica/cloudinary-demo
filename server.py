4ebceaf463781c520b69a3b04eae12658fb26fdf
Heather Mahan <heatheramahan@gmail.com>
2021-11-21 03:59:32 +0000

    AJAX demo with JQuery

--- server.py
+++ server.py
@@ -4,13 +4,38 @@ import os
 
 CLOUDINARY_KEY = os.environ['CLOUDINARY_KEY']
 CLOUDINARY_SECRET = os.environ['CLOUDINARY_SECRET']
-CLOUD_NAME = "YOUR-CLOUD-NAME-HERE"
+CLOUD_NAME = "YOUR-CLOUDINARY-NAME-HERE"
 
 app = Flask(__name__)
 
+def save_to_cloudinary(my_file):
+    result = cloudinary.uploader.upload(my_file, 
+                                        api_key=CLOUDINARY_KEY, 
+                                        api_secret=CLOUDINARY_SECRET, 
+                                        cloud_name=CLOUD_NAME)
+
+    img_url = result['secure_url']
+    print('*' * 20)
+    print("SAVE THIS url to your database!!")
+    # Save this url to your database, so that you can show the image to the user later.
+    print(img_url)
+    print('*' * 20)
+    return img_url
+
+
 @app.route('/')
-def homepage():$
+def homepage():
     return render_template('homepage.html')
+
+@app.route('/ajax-media-upload-form')
+def show_ajax_form():
+    return render_template('form-ajax.html')  
+
+@app.route('/post-form-data-ajax', methods=['POST'])
+def post_form_data_ajax():
+    my_file = request.files['my-file']
+    img_url = save_to_cloudinary(my_file)
+    return img_url
 
 @app.route('/show-image')
 def show_image():
@@ -26,17 +51,8 @@ def post_form_data():
 
     my_file = request.files['my-file']
 
-    result = cloudinary.uploader.upload(my_file, 
-                                        api_key=CLOUDINARY_KEY, 
-                                        api_secret=CLOUDINARY_SECRET, 
-                                        cloud_name=CLOUD_NAME)
+    img_url = save_to_cloudinary(my_file)
 
-    img_url = result['secure_url']
-    print('*' * 20)
-    print("SAVE THIS url to your database!!")
-    # Save this url to your database, so that you can show the image to the user later.
-    print(img_url)
-    print('*' * 20)
     return redirect(url_for('show_image', imgURL=img_url))
 
 if __name__ == '__main__':
